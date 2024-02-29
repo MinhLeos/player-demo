44a2b3b356736b619b555b3a317e6d5df93caf67
Minh Le <minh.le@mgisolutions.com>
2024-02-29 12:26:49 +0700

    page

---
+++ dist/assets/vidstack-audio-6hP_mAqE.js
@@ -0,0 +1,1 @@
+import{H as t}from"./vidstack-html-mYkEUDYE.js";import"./index-D8bBdHoz.js";import"./vidstack-BhYx9Fjk-CP52TKrN.js";class o extends t{constructor(){super(...arguments),this.$$PROVIDER_TYPE="AUDIO"}get type(){return"audio"}setup(){super.setup(),this.type==="audio"&&this.b.delegate.c("provider-setup",this)}get audio(){return this.a}}export{o as AudioProvider};
