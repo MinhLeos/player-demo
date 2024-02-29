5793a01a66569f540d93910755a1704157f034df
Minh Le <minh.le@mgisolutions.com>
2024-02-29 12:24:38 +0700

    Updates

---
+++ assets/vidstack-BhYx9Fjk-CP52TKrN.js
@@ -0,0 +1,1 @@
+import{g as i,K as t}from"./index-D8bBdHoz.js";class r{constructor(s){this.Ca=s}Bb(){i(this.ca)&&this.Oe()}ra(){t(this.ca)&&window.cancelAnimationFrame(this.ca),this.ca=void 0}Oe(){this.ca=window.requestAnimationFrame(()=>{i(this.ca)||(this.Ca(),this.Oe())})}}export{r as R};
